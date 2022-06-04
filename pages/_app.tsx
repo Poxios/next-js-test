c7139c4eb1fddc59152a504b1cec746c559ac126
Poxios <poxios0310@gmail.com>
2022-06-04 14:35:07 +0900

    added corona template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import '../styles/css/style.css'
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Script from "next/script";
@@ -7,12 +8,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link
+      {/*   <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossOrigin="anonymous"
-        />
+        /> */}
       </Head>
       <Script id="bootstrap-cdn" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
       <Component {...pageProps} />
